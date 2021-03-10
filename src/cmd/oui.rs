f5f24d8c3dff433b6ebd52b271e87e64f990f66d
Louis Thiery <thiery.louis@gmail.com>
2021-03-09 17:28:41 -0800

    oui fix

--- src/cmd/oui.rs
+++ src/cmd/oui.rs
@@ -148,7 +148,7 @@ fn print_txn(
         OutputFormat::Table => {
             ptable!(
                 ["Key", "Value"],
-                ["Requested OUI", txn.oui + 1],
+                ["Requested OUI", txn.oui],
                 ["Reqeuested Subnet Size", txn.requested_subnet_size],
                 [
                     "Addresses",
@@ -161,7 +161,7 @@ fn print_txn(
         }
         OutputFormat::Json => {
             let table = json!({
-                "requested_oui": txn.oui + 1,
+                "requested_oui": txn.oui,
                 "addresses": map_addresses(txn.addresses.clone(), |v| v.to_string())?,
                 "requested_subnet_size": txn.requested_subnet_size,
                 "hash": status_json(status),
